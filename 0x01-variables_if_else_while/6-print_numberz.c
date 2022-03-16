106d6e88326e13658cd50467310891fa1347371c
Judith <dickjudth67@gmail.com>
2022-03-16 13:20:41 -0700

    this commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * program that prints all single numbers of base ten
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
