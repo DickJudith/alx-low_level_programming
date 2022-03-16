331df702f0f91ea9b548144e15ce0cd04bc2bc6d
Judith <dickjudth67@gmail.com>
2022-03-16 13:55:31 -0700

    this commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * program that prints alphabets in reverse order
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
