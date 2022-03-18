329cc54b6d6970010196d342a255fae03cd80b7e
Judith <dickjudth67@gmail.com>
2022-03-18 06:04:10 -0700

    this commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- program that prints followed by a new line
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
