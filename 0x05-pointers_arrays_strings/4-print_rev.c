df38368cb208e65e372517d31269323ba1970511
Judith <dickjudth67@gmail.com>
2022-03-21 21:07:25 -0700

    this commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print reverse characters
+ * @s: validate the character
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	string a = "lobby";
+
+	reverse(a);
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - print reverse characters
+ *
+ * @s: validate the character
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int i = 0, a = a, Aux;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i > a)
+	{
+		Aux = s[i];
+		s[i--] = s[a];
+		s[a++] = Aux;
+	}
+	return (0);
+}
