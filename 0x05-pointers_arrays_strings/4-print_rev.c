22ac9616c2e4d122d7a5c8029add96e289e58895
DickJudith <dickjudith67@gmail.com>
2022-04-18 05:37:24 -0700

    this commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,19 @@
  * Return: Always 0
  */
 
-int main(void)
+void print_rev(char *s)
 {
-	string a = "lobby";
+	int i = 0;
 
-	reverse(a);
-
-	return (0);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
 }
