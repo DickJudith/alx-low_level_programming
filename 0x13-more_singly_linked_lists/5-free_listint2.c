0b0bb29735ec091f041e0ce56f580e7e131891c0
DickJudith <dickjudith67@gmail.com>
2022-04-27 21:09:41 -0700

    this commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,17 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *ten;
 
-	while (head)
+	if (head != NULL)
 	{
-		temp = *head;
-		*head = head->next;
-		free(temp);
+		ten = *head;
+
+		while ((temp = ten) != NULL)
+		{
+			ten = ten->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
-	head = NULL;
 }
