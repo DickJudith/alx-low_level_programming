171de1505116094ac3ba1f31426128857665f122
DickJudith <dickjudith67@gmail.com>
2022-04-28 09:01:19 -0700

    this commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - function that converts binary to uint
+ * @b: character
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+}
